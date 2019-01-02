c5d8ec6293d79b573a87cfc7f0beac77813f199d
Kevin J. Dugan <dugankj@ornl.gov>
2019-01-02 13:23:32 -0500

    Moving Message send and receive to separate functions

--- test.cpp
+++ test.cpp
@@ -5,6 +5,9 @@
 #include "Teuchos_DefaultMpiComm.hpp"
 #include <Teuchos_XMLParameterListHelpers.hpp>
 
+void sendMessage(const std::stringstream &messageStream, const MPI_Comm &io_comm);
+void recvMessage(const std::stringstream &messageStream, const MPI_Comm &io_comm);
+
 int main (){
 
   MPI_Init(NULL, NULL);
@@ -22,52 +25,50 @@ int main (){
   if (world_comm_rank != 0){
     std::stringstream ss;
     ss << "Process " << world_comm_rank << ", Density: " << verain->sublist("CORE").sublist("Materials").sublist("Material_pyrex").get<double>("density") << std::endl;
-
-    int messageLength = ss.str().length();
-    char message[messageLength+1];
-    strcpy(message, ss.str().c_str());
-
-    MPI_Send(message, messageLength+1, MPI_CHAR, 0, 0, MPI_COMM_WORLD);
+    sendMessage(ss, MPI_COMM_WORLD);
   }else {
     std::stringstream ss;
     ss << "Process " << world_comm_rank << ", Density: " << verain->sublist("CORE").sublist("Materials").sublist("Material_pyrex").get<double>("density") << std::endl;
-    int messageLength = ss.str().length();
-    char message[messageLength+1];
-    strcpy(message, ss.str().c_str());
-    std::cout << message << std::flush;
-
-    for (int process = 1; process < world_comm_size; process++){
-      MPI_Status status;
-      MPI_Recv(message, messageLength+1, MPI_CHAR, process, 0, MPI_COMM_WORLD, &status);
-      std::cout << message << std::flush;
-    }
+    recvMessage(ss, MPI_COMM_WORLD);
   }
 
 
   if (world_comm_rank != 0){
     std::stringstream ss;
     ss << "World CPU: " << world_comm_rank << "/" << world_comm_size << std::endl;
-
-    int messageLength = ss.str().length();
-    char message[messageLength+1];
-    strcpy(message, ss.str().c_str());
-
-    MPI_Send(message, messageLength+1, MPI_CHAR, 0, 0, MPI_COMM_WORLD);
+    sendMessage(ss, MPI_COMM_WORLD);
   } else {
     std::stringstream ss;
     ss << "World CPU: " << world_comm_rank << "/" << world_comm_size << std::endl;
-    int messageLength = ss.str().length();
-    char message[messageLength+1];
-    strcpy(message, ss.str().c_str());
-    std::cout << message << std::flush;
-
-    for (int process = 1; process < world_comm_size; process++){
-      MPI_Status status;
-      MPI_Recv(message, messageLength+1, MPI_CHAR, process, 0, MPI_COMM_WORLD, &status);
-      std::cout << message << std::flush;
-    }
+    recvMessage(ss, MPI_COMM_WORLD);
   }
 
   MPI_Finalize();
   return 0;
+}
+
+
+void sendMessage(const std::stringstream &messageStream, const MPI_Comm &io_comm){
+  int messageLength = messageStream.str().length();
+  char message[messageLength+1];
+  strcpy(message, messageStream.str().c_str());
+
+  MPI_Send(message, messageLength+1, MPI_CHAR, 0, 0, MPI_COMM_WORLD);
+}
+
+void recvMessage(const std::stringstream &messageStream, const MPI_Comm &io_comm){
+  int world_comm_rank, world_comm_size;
+  MPI_Comm_rank(MPI_COMM_WORLD, &world_comm_rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &world_comm_size);
+
+  int messageLength = messageStream.str().length();
+  char message[messageLength+1];
+  strcpy(message, messageStream.str().c_str());
+  std::cout << message << std::flush;
+
+  for (int process = 1; process < world_comm_size; process++){
+    MPI_Status status;
+    MPI_Recv(message, messageLength+1, MPI_CHAR, process, 0, MPI_COMM_WORLD, &status);
+    std::cout << message << std::flush;
+  }
 }
