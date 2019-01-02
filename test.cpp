3b9075e4ebab2d21b7959a7d6c13f6d43e775f62
Kevin J. Dugan <dugankj@ornl.gov>
2019-01-02 13:32:08 -0500

    Updating message function communicator

--- test.cpp
+++ test.cpp
@@ -53,22 +53,21 @@ void sendMessage(const std::stringstream &messageStream, const MPI_Comm &io_comm
   char message[messageLength+1];
   strcpy(message, messageStream.str().c_str());
 
-  MPI_Send(message, messageLength+1, MPI_CHAR, 0, 0, MPI_COMM_WORLD);
+  MPI_Send(message, messageLength+1, MPI_CHAR, 0, 0, io_comm);
 }
 
 void recvMessage(const std::stringstream &messageStream, const MPI_Comm &io_comm){
-  int world_comm_rank, world_comm_size;
-  MPI_Comm_rank(MPI_COMM_WORLD, &world_comm_rank);
-  MPI_Comm_size(MPI_COMM_WORLD, &world_comm_size);
+  int comm_size;
+  MPI_Comm_size(io_comm, &comm_size);
 
   int messageLength = messageStream.str().length();
   char message[messageLength+1];
   strcpy(message, messageStream.str().c_str());
   std::cout << message << std::flush;
 
-  for (int process = 1; process < world_comm_size; process++){
+  for (int process = 1; process < comm_size; process++){
     MPI_Status status;
-    MPI_Recv(message, messageLength+1, MPI_CHAR, process, 0, MPI_COMM_WORLD, &status);
+    MPI_Recv(message, messageLength+1, MPI_CHAR, process, 0, io_comm, &status);
     std::cout << message << std::flush;
   }
 }
